0bd3a770d313240062e1295100010c57886c5773
Eric <devel010@umn.edu>
2020-02-11 18:56:11 -0600

    Run animation turning on and off

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,12 +8,21 @@ public class Player : MonoBehaviour
     [SerializeField] float runSpeed = 10f;
 
     Rigidbody2D myRigidBody;
+    Animator animator;
 
 
     private void Run() {
         float controlThrow = Input.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow*runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        animator.SetBool("Running", true);
+    }
+
+    private void UpdateAnimationState() {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (!playerHasHorizontalSpeed) {
+            animator.SetBool("Running", false);
+        }
     }
 
     private void FlipSprite(){
@@ -26,6 +35,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -34,5 +44,6 @@ public class Player : MonoBehaviour
     {
         Run();
         FlipSprite();
+        UpdateAnimationState();
     }
 }
