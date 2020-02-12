836df71a5cfb7a6e0365e3f0ec79ed65e8c6a4d5
Eric <devel010@umn.edu>
2020-02-11 18:23:59 -0600

    HOrizontal player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] float runSpeed = 10f;
+    float xMinimum;
+    float xMaximum;
+
+    Rigidbody2D myRigidBody;
+
+
+    // private void Move() {
+    //     var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * runSpeed;
+    //     var newXPosition = Mathf.Clamp(transform.position.x + deltaX, xMinimum, xMaximum);
+    //     transform.position = new Vector2(newXPosition, transform.position.y);
+    // }
+
+    private void Run() {
+        float controlThrow = Input.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow*runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
     }
 }
