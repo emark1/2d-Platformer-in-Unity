7fcb5937e625e690c75efb85872eec2508a2d33c
Eric <devel010@umn.edu>
2020-02-13 16:38:38 -0600

    Climbing animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,23 @@ public class Player : MonoBehaviour
     //Variables
     Rigidbody2D myRigidBody;
     Animator animator;
+    Collider2D myCollider;
+    LayerMask groundLayer;
+    LayerMask ladderLayer;
 
     //Functions
     void Start()
     {
+        ladderLayer = LayerMask.GetMask("Ladder");
+        groundLayer = LayerMask.GetMask("Ground");
         animator = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
     }
 
     void Update()
     {
+        Climb();
         Jump();
         Run();
         FlipSprite();
@@ -37,9 +44,21 @@ public class Player : MonoBehaviour
     }
 
     private void Jump() {
-        if (Input.GetButtonDown("Jump")) {
-            Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
-            myRigidBody.velocity += jumpVelocity;
+        if (myCollider.IsTouchingLayers(groundLayer)) {
+            if (Input.GetButtonDown("Jump")) {
+                Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+                myRigidBody.velocity += jumpVelocity;
+            }
+        }
+    }
+
+    private void Climb() {
+        if (myCollider.IsTouchingLayers(ladderLayer)) {
+            animator.SetBool("Climbing", true);
+
+            float controlThrow = Input.GetAxis("Vertical");
+            Vector2 climbVelocity = new
+
         }
     }
 
@@ -47,6 +66,10 @@ public class Player : MonoBehaviour
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         if (!playerHasHorizontalSpeed) {
             animator.SetBool("Running", false);
+        }
+
+        if (!myCollider.IsTouchingLayers(ladderLayer)) {
+            animator.SetBool("Climbing", false);
         }
     }
 
