8f5d24ad81feb3cf112cad4cfcef6a28fb46db06
Eric <devel010@umn.edu>
2020-05-04 17:57:20 -0500

    Proper SFX

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -7,6 +7,7 @@ public class CoinPickup : MonoBehaviour
     [SerializeField] AudioClip coinPickupSFX;
 
     private void OnTriggerEnter2D(Collider2D collision) {
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
